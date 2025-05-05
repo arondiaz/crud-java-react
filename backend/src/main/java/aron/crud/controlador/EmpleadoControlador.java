baef2692c4a2ce6058ab22d46d50108aebccef25
Arón Díaz <arondiaz.cpn@gmail.com>
2025-05-05 19:45:02 -0300

    implemented controller to add employee

--- backend/src/main/java/aron/crud/controlador/EmpleadoControlador.java
+++ backend/src/main/java/aron/crud/controlador/EmpleadoControlador.java
@@ -5,10 +5,7 @@ import aron.crud.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,12 @@ public class EmpleadoControlador {
         var empleados = empleadoServicio.listarEmpleados();
         empleados.forEach((empleado -> logger.info(empleado.toString())));
         return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        logger.info("Empleado agregar" + empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
     }
 
 
